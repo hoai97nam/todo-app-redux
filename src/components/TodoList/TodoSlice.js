3a19c5ac3fa5d1bd6867f770783eb364c09c1ba6
Nam <nhnam97@gmail.com>
2022-06-15 09:05:58 +0700

    update todo slice

--- src/components/TodoList/TodoSlice.js
+++ src/components/TodoList/TodoSlice.js
@@ -1,16 +1,16 @@
 const initState = [
-    { id: 1, name: 'learn something', completed: true, priority: 'high' },
-    { id: 2, name: 'learn somewhere', completed: true, priority: 'medium' },
-    { id: 3, name: 'learn somehow', completed: true, priority: 'low' }
+    { id: 1, name: 'learn something', completed: true, priority: 'High' },
+    { id: 2, name: 'learn somewhere', completed: true, priority: 'Medium' },
+    { id: 3, name: 'learn somehow', completed: true, priority: 'Low' }
 ]
 
 const todoReducer = (state = initState, action) => {
     switch (action.type) {
         case 'todoList/addTodo':
             return [
-                ...state.todoList,
+                ...state,
                 action.payload
-            ]
+            ];
         default:
             return state;
     }
